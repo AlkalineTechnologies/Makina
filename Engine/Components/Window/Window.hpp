60141ca41942865d5a6c9c690d7c17dc4ebd3b07
plasmarad <73721329+plasmarad@users.noreply.github.com>
2024-02-27 23:50:53 -0500

    Delete Engine directory

--- Engine/Components/Window/Window.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __ENGINE_WINDOW_HPP__
-#define __ENGINE_WINDOW_HPP__
-
-#include "GLFW/glfw3.h"
-
-
-
-#endif // __ENGINE_WINDOW_HPP__
--- Engine/Engine.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "Engine.hpp"
-
-// definitions here...
-namespace Engine {
-}
