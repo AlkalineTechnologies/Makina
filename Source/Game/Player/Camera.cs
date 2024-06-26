fab8fff9f244a45523c58a272a5b277b5d3d0743
plasmarad <userofgogle356@gmail.com>
2024-06-26 12:19:19 -0400

    Uploading files to the repository

---
+++ Source/Game/Player/Camera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FlaxEngine;
+
+namespace Game;
+
+public class Camera : Script
+{
+    [Header("Actors")]
+    public Actor CameraActor;
+    public Actor Player;
+    [Header("Camera")]
+    public float CameraXSpeed = 10f;
+    public float CameraYSpeed = 10f;
+    public float MouseX;
+    public float MouseY;
+    public override void OnEnable(){ 
+        Screen.CursorLock = CursorLockMode.Locked;
+        Screen.CursorVisible = false;
+
+    }
+    public override void OnUpdate()
+    {
+        MouseX += Input.GetAxis("Mouse X") * CameraXSpeed * Time.DeltaTime;
+        MouseY += Input.GetAxis("Mouse Y") * CameraYSpeed  * Time.DeltaTime;
+        MouseX = Math.Clamp(MouseX, -90, 90);
+        
+        if (Input.GetKey(KeyboardKeys.Escape)){
+            Screen.CursorLock = CursorLockMode.Locked;
+            Screen.CursorVisible = false;
+        }
+        
+        Player.LocalOrientation = Quaternion.Euler(0, MouseY, 0);
+        CameraActor.EulerAngles = new Vector3 (MouseX,Player.LocalEulerAngles.Y, 0);
+    }
+}
---
+++ Source/Game/Player/Interact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Interact Script.
+/// </summary>
+public class Interact : Script
+{
+
+    public Actor Camera;
+    public LayersMask mask;
+    public float ReachDistance;
+
+    public override void OnUpdate() {
+        RayCastHit hit;
+        if (Physics.RayCast(Camera.Position, Camera.Transform.Forward, out hit, ReachDistance, mask)) {
+        
+        }
+    }
+}
