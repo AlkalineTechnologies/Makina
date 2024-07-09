a08ed035f8fe5bdf5ddcf5fc8ef24ead039a66f4
plasmarad <userofgogle356@gmail.com>
2024-07-09 12:22:35 -0400

    Cursor unlock now works unlike before.

--- Source/Game/Player/Camera.cs
+++ Source/Game/Player/Camera.cs
@@ -8,11 +8,13 @@ public class Camera : Script
     [Header("Actors")]
     public Actor CameraActor;
     public Actor Player;
+
     [Header("Camera")]
     public float CameraXSpeed = 10f;
     public float CameraYSpeed = 10f;
     public float MouseX;
     public float MouseY;
+    
     public override void OnEnable () { 
         Screen.CursorLock       = CursorLockMode.Locked ;
         Screen.CursorVisible    = false                 ;
@@ -24,9 +26,10 @@ public class Camera : Script
         
         MouseX = Math.Clamp(MouseX, -90, 90);
         
+        //! ONLY FOR TESTING
         if ( Input.GetKey( KeyboardKeys.Escape ) ) {
-            Screen.CursorLock       = CursorLockMode.Locked ;
-            Screen.CursorVisible    = false                 ;
+            Screen.CursorLock       = CursorLockMode.None ;
+            Screen.CursorVisible    = true                ;
         }
         
         Player.LocalOrientation = Quaternion.Euler(0, MouseY, 0);
