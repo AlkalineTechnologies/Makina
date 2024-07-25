8b6d6a4ff89dc7b3ddef8614b195926511caeaf6
plasmarad <userofgogle356@gmail.com>
2024-07-24 21:00:45 -0400

    Brokedown Character Movement script into multiple "sub-scripts" that handle Input, UI, Stamina, and State Management.

--- Source/Game/Player/PlayerController.cs
+++ Source/Game/Player/PlayerController.cs
@@ -7,7 +7,7 @@ using FlaxEngine.GUI;
 namespace Game;
 
 /// <summary>
-/// MovementsMaster Script.
+/// Script in charge of the player's movement.
 /// </summary>
 public class PlayerController : Script
 {
@@ -26,56 +26,27 @@ public class PlayerController : Script
 
     [Header("Movement")]
 
-    public  float            MovementSpeed           = 250f                 ;
-    public  float            RunSpeedMultiplier      = 1.2f                 ;
-    public  float            MovementAirMultiplier   = 1.5f                 ;
-    public  float            CrouchSpeedMultiplier    = 0.7f                ;
-    public  float            CrouchHeight            = 125                  ;
-    private Vector3          Direction               = new Vector3(0,0,0)   ;
-    private Vector3          _PrevPosition           = new Vector3(0,0,0)   ;
+    public  float            MovementSpeed         { get; set; }  = 250f                ;
+    public  float            RunSpeedMultiplier    { get; set; }  = 1.2f                ;
+    public  float            MovementAirMultiplier { get; set; }  = 1.5f                ;
+    public  float            CrouchSpeedMultiplier { get; set; }  = 0.7f                ;
+    public  float            CrouchHeight          { get; set; }  = 125                 ;
+    private Vector3          PushForce             { get; set; }  = Vector3.Zero        ;
+    private Vector3          _PrevPosition         { get; set; }  = Vector3.Zero        ;
     
 
     [Header("Inputs")]
-
-    private bool    JumpInput           ;
-    public  bool    RunningInput        ;
-    public  bool    CrouchInput         ;
-    private float   VerticalInput       ;
-    private float   HorizontalInput     ;
-
+    public PlayerInputSystem InputSystem    { get; set; }
     
-
-
-    [Header("Jumping")]
-
-    //! FOR THE LOVE OF GOD. DO NOT TOUCH THESE SETTINGS UNLESS
-    //! YOU ARE A MORON OR A MATH MASSOCIST.
-    public  double      JumpForce           = 125           ;
-    public  int         JumpCooldown        = 250           ;
-    public  double      JumpPeakTime        = 0.45          ;
-    private Vector3     JumpDisplacement    = Vector3.Zero  ;
+    [Header("UI")]
+    public  PlayerHUD       HUD             { get; set; }
 
     [Header("Stamina")]
-
-    public  float   Stamina                     = 100       ; // cannot be 0 otherwise player stutters
-    public  float   MaxStamina                  = 100       ;
-    public  float   StaminaRegenTime            = 10000     ;
-    public  int     StaminaRegenTimeout         = 1000      ;
-
-    // CONSUMPTIONS:
-    public  int     StaminaJumpConsumption      = 40    ;
-    public  int     StaminaStrafeConsumption    = 5     ;
+    public StaminaManager   StaminaManager  { get; set; }
     
-    
+
     [Header("State Management")]
-    // STATUS:
-    private bool    Sliding                     ;
-    private bool    CanAirStrafe                ;
-    public  bool    RegenningStamina            ;
-    public  bool    UsingStamina                ;
-    public  bool    Crouching                   ;
-    public  bool    Jumping                     ;
-    public  bool    IsGrounded                  ;
+    public PlayerStateMachine StateMachine  { get; set; }
 
 
     public override void OnEnable       () {
@@ -97,73 +68,63 @@ public class PlayerController : Script
     }
 
     public override void OnUpdate       () { 
-        HandleInput     (); 
         MovePlayer      ();   
         UpdateJump      ();
-        StaminaManager  ();
         CrouchManager   ();
 
         
 
         // * Update UI
-        StaminaBar .Get<ProgressBar> ().Value    = Stamina / MaxStamina                              ;
-        DEBUG_SPEED.Get<Label>       ().Text     = "Speed: " + ( (int) CalculateSpeed() ).ToString() ;
+        DEBUG_SPEED.Get<Label>().Text     = "Speed: " + ( (int) CalculateSpeed() ).ToString() +
+                                            "\t FPS: " + Math.Round(1 / Time.DeltaTime).ToString();
         
     }
     
-    private void HandleInput            () {
-
-        JumpInput           = Input.GetAction   (   "Jump"          );
-        RunningInput        = Input.GetAction   (   "Run"           );
-        VerticalInput       = Input.GetAxis     (   "Vertical"      );
-        HorizontalInput     = Input.GetAxis     (   "Horizontal"    );
-        HorizontalInput     = Input.GetAxis     (   "Horizontal"    );
-        CrouchInput         = Input.GetAction   (   "Crouch"        );
-
-        if (CrouchInput) Crouching = !Crouching;
-    }
+    
 
     void MovePlayer(){
-        
-
-        Direction = ( Transform.Forward * VerticalInput ) + ( Transform.Right * HorizontalInput );
-        Direction.Normalize();
-        
-        if ( JumpInput  )   JumpDispatch    ();
-        
-        
-        UsingStamina = UsingStamina ? true : RunningInput;
-
-        // Multipliers for speed
-        Direction   *= MovementSpeed * (RunningInput ? RunSpeedMultiplier : 1) * ( !IsGrounded ?  MovementAirMultiplier : 1) * (Crouching ? CrouchSpeedMultiplier : 1);
-
-
-
-        Controller.Move(
-            ( Direction + JumpDisplacement ) * ( (Stamina > 0 || IsGrounded) ? Time.DeltaTime : 0 )
-        );
-
-    }
+            
+            Vector3 Direction = ( Transform.Right *  InputSystem.MovementInput.X) + ( Transform.Forward *  InputSystem.MovementInput.Z);
+            Direction.Normalize();
+    
+            Debug.Log("Normalized: " + Direction);
+    
+            if ( InputSystem.JumpInput  )   JumpDispatch    ();
+            
+            
+            StateMachine.UsingStamina = StateMachine.UsingStamina || StateMachine.Running;
+    
+            // Multipliers for speed
+            Direction   *=                      MovementSpeed                * 
+                (StateMachine.Running       ?   RunSpeedMultiplier      : 1) * 
+                (!StateMachine.IsGrounded   ?   MovementAirMultiplier   : 1) * 
+                (StateMachine.Crouching     ?   CrouchSpeedMultiplier   : 1) ;
+    
+            Controller.Move(
+                ( Direction + PushForce ) * Time.DeltaTime
+            );
+    
+        }
 
     private void CrouchManager () {
 
         float LerpSpeed = Time.DeltaTime * 10;
 
-        Controller.Height = Crouching ? CrouchHeight : 180;
+        Controller.Height = StateMachine.Crouching ? CrouchHeight : 180;
         
         Actor.Scale = Vector3.Lerp(
             Actor.Scale, 
-            new Vector3(1, Crouching ? (CrouchHeight/180) : 1, 1), 
+            new Vector3(1, StateMachine.Crouching ? (CrouchHeight/180) : 1, 1), 
             LerpSpeed
         );
-        // TODO: SETUP CROUCH TOGGLE
 
     }
 
-    private float jumpTimeElapsed;
+
     private void JumpDispatch() {
         // When you can't jump: no stamina, not grounded, and not within coyote time
-        if (Stamina - StaminaJumpConsumption <= 0 && !IsGrounded) {
+        if (StaminaManager.Stamina - StaminaManager.StaminaJumpConsumption <= 0 && !StateMachine.IsGrounded) {
+            //! TODO FOR TOMORROW: make this into a function within PlayerHUD 
             // Play SFX and flash the stamina bar
             StaminaBar.Get<ProgressBar>().BarColor = Color.Red;
             
@@ -176,90 +137,16 @@ public class PlayerController : Script
             
             return;
         }
+        
+        //! REMOVED for now in order to reimplement
+        //! a proper jump system.
 
-        Jumping              = true  ;
-        Stamina             -= StaminaJumpConsumption * (IsGrounded ? 0 : 1);
-        UsingStamina         = UsingStamina ? true : !IsGrounded;
-        jumpTimeElapsed      = 0;
-        Gravity.Multiplier   = 0;
 
     }
 
     private void UpdateJump() {
-        if (!Jumping) return;
-
-        Crouching = false;
-
-        jumpTimeElapsed += Time.DeltaTime;  
-        
-        double progress  = jumpTimeElapsed / JumpPeakTime;
-
-        double force     = 4d * (3.33d/JumpPeakTime * JumpForce) * progress * (1-progress);
-
-        if (progress    >= 0.5) force *= -1;
-
-        JumpDisplacement = Transform.Up * force;
-
-        if (jumpTimeElapsed >= JumpPeakTime) {
-            Jumping             = false;
-            jumpTimeElapsed     = 0;
-            JumpDisplacement    = Vector3.Zero;
-            Gravity.Multiplier  = 1;
-        }
-    }
-
-    private void StaminaManager () {
-        
-        if (!RegenningStamina)
-            _ = RegenStamina();
-        
-        CanAirStrafe = !IsGrounded && Stamina > 0 && (HorizontalInput != 0 || VerticalInput != 0);
-
-        if (!CanAirStrafe) return;
-        
-        // This code manages the deduction of air strafes based on certain conditions:
-        // * If the player is jumping
-        // * If the player is running (this increases the rate of air strafe consumption)
-        // * If the player is moving (normal rate of air strafe consumption)
-        // * If the player is grounded (no air strafes are consumed)
-        // The resulting air strafe count is clamped between 0 and AirStrafeMax.
-        UsingStamina = true;
-        Stamina = Math.Clamp(
-            // The base air strafe count is reduced by a consumption rate, which is modified by various factors.
-            Stamina - ( 
-                    
-                    ( CanAirStrafe && RunningInput ? (RunSpeedMultiplier * StaminaStrafeConsumption) : StaminaStrafeConsumption ) * Time.DeltaTime
-                
-                ),
-            // The resulting value is clamped between 1 and AirStrafeMax.
-            1, MaxStamina
-        );
-        
-        
-    }
-
-    private async Task RegenStamina () {
-        if (RegenningStamina) return;
-
-        RegenningStamina = true;
-
-        await Task.Delay( TimeSpan.FromMilliseconds ( StaminaRegenTimeout ) );
-        
-        int x =  (int) Math.Round( StaminaRegenTime / MaxStamina );
-
-        while ( Stamina < MaxStamina ) {
-        
-            if ( UsingStamina ) { UsingStamina = false; break; }
-            
-            Stamina++;
-        
-            await Task.Delay( TimeSpan.FromMilliseconds ( x ) );
-
-        }
-        
-        RegenningStamina = false;
-        Stamina = Mathf.Clamp(Stamina, 1, MaxStamina);
-
+        //! REMOVED for now in order to reimplement
+        //! a proper jump system.
     }
 
     float CalculateSpeed () {
@@ -268,8 +155,8 @@ public class PlayerController : Script
         return Speed;
     }
 
-    void GroundCheckEnter (Collision collision) { IsGrounded = true ; }
+    void GroundCheckEnter (Collision collision) { StateMachine.IsGrounded = true ; }
 
-    void GroundCheckExit  (Collision collision) { IsGrounded = false; }
+    void GroundCheckExit  (Collision collision) { StateMachine.IsGrounded = false; }
 
 }
---
+++ Source/Game/Player/StaminaManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using FlaxEngine;
+using FlaxEngine.Utilities;
+
+namespace Game;
+
+/// <summary>
+/// StaminaManager Script.
+/// </summary>
+public class StaminaManager : Script{
+    public PlayerStateMachine StateMachine      { get; set; }
+    public PlayerController MovementController  { get; set; }
+
+    public float Stamina                        { get; set; }   = 100   ;
+    public float MaxStamina                     { get; set; }   = 100   ;
+    public float StaminaRegenTime               { get; set; }   = 10000 ;
+    public int StaminaRegenTimeout              { get; set; }   = 1000  ;
+
+    // CONSUMPTIONS:
+    public int StaminaJumpConsumption           { get; set; }   = 40    ;
+    public int StaminaStrafeConsumption         { get; set; }   = 5     ;
+
+    public override void OnUpdate()
+    {
+
+        if (!StateMachine.RegenningStamina)
+            _ = RegenStamina();
+
+        StateMachine.CanAirStrafe = !StateMachine.IsGrounded && Stamina > 0;
+
+        if (!StateMachine.CanAirStrafe) return;
+
+        // This code manages the deduction of air strafes based on certain conditions:
+        // * If the player is jumping
+        // * If the player is running (this increases the rate of air strafe consumption)
+        // * If the player is moving (normal rate of air strafe consumption)
+        // * If the player is grounded (no air strafes are consumed)
+        // The resulting air strafe count is clamped between 0 and AirStrafeMax.
+        StateMachine.UsingStamina = true;
+        Stamina = Math.Clamp(
+            // The base air strafe count is reduced by a consumption rate, which is modified by various factors.
+            Stamina - (
+
+                    (StateMachine.CanAirStrafe && StateMachine.Running ?
+                        (MovementController.RunSpeedMultiplier * StaminaStrafeConsumption) : StaminaStrafeConsumption
+                    ) * Time.DeltaTime
+
+                ),
+            // The resulting value is clamped between 1 and AirStrafeMax.
+            1, MaxStamina
+        );
+
+
+    }
+
+    private async Task RegenStamina()
+    {
+        if (StateMachine.RegenningStamina) return;
+
+        StateMachine.RegenningStamina = true;
+
+        await Task.Delay(TimeSpan.FromMilliseconds(StaminaRegenTimeout));
+
+        int x = (int)Math.Round(StaminaRegenTime / MaxStamina);
+
+        while (Stamina < MaxStamina)
+        {
+
+            if (StateMachine.UsingStamina) { StateMachine.UsingStamina = false; break; }
+
+            Stamina++;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(x));
+
+        }
+
+        StateMachine.RegenningStamina = false;
+
+        Stamina = Mathf.Clamp(Stamina, 1, MaxStamina);
+
+    }
+}
